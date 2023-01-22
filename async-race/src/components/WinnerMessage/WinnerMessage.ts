74b2d8fa9b83c45ecfd4e77f6d7225aced452e66
lgklsv <kolosov20011@gmail.com>
2023-01-22 22:39:11 +0300

    fix: add capitalize function to show capitalized car name on winner message screen

--- async-race/src/components/WinnerMessage/WinnerMessage.ts
+++ async-race/src/components/WinnerMessage/WinnerMessage.ts
@@ -1,3 +1,4 @@
+import { capitalize } from '../../utils/capitalize';
 import { createElem } from '../../utils/element-render/create-element';
 import { renderButton } from '../Button/Button';
 import { resetAllHandler } from '../Garage/components/Controls/components/CarControlBtns/ControlHandlers/ResetAllHandler';
@@ -9,7 +10,7 @@ export const renderWinnerMessage = (winner: RaceWinner): HTMLElement => {
   const winnersMesContainer: HTMLElement = createElem('div', 'winner-message__container');
 
   const message: HTMLElement = createElem('h2', 'winner-message__car');
-  message.innerHTML = `${winner.winner.name} wins in (${winner.winnerTime}s) ⏱️`;
+  message.innerHTML = `${capitalize(winner.winner.name)} wins in (${winner.winnerTime}s) ⏱️`;
 
   const resetBtn: HTMLElement = renderButton('reset', '', ['close-message']);
   resetBtn.onclick = resetAllHandler;
